9d71a9bb1d06abda6a939966e72f506b57b22496
Blankman <60937724+jkblank@users.noreply.github.com>
2022-02-13 19:25:20 +0200

    Update main.py
    Binary Calculator added

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello world!")
+inputvar = int(input( "Input a number to converto to binary: "))
+outputvar=""
+while (inputvar>0):
+    if(inputvar%2==1):
+        outputvar="1"+outputvar
+        inputvar=(inputvar-1)/2
+    else:
+        outputvar="0"+outputvar
+        inputvar=(inputvar)/2
+
+print("Binary is: " + outputvar)
